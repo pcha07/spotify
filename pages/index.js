d43a53d9d36a10f765a947daa3dc174b52c68fd4
Prabh Chahal <chahalprabh10@gmail.com>
2022-05-12 15:36:23 -0700

    auth fix

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,15 @@
 import { useSession } from "next-auth/react";
 import Head from "next/head";
-import Image from "next/image";
 import { useRouter } from "next/router";
 import Dashboard from "../components/Dashboard";
 import Loader from "../components/Loader";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const router = useRouter();
   const { status, data: session } = useSession({
     required: true,
     onUnauthenticated() {
-      router.push("/auth/signIn");
+      router.push("/auth/signin");
     },
   });
 
