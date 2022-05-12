491d2f836e21ce54d34ac94cf60e5e4669f35a54
Prabh Chahal <chahalprabh10@gmail.com>
2022-05-12 15:39:09 -0700

    auth fix

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   const { status, data: session } = useSession({
     required: true,
     onUnauthenticated() {
-      router.push("/auth/signin");
+      router.push("/auth/signIn");
     },
   });
 
